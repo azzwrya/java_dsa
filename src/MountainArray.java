07c4703cf130ce563e5105054af65859065b3cfc
azzwrya <aishxs2002@gmail.com>
2024-06-14 00:28:55 +0530

    last commit

---
+++ src/MountainArray.java
@@ -0,0 +1,25 @@
+public class MountainArray {
+    public static void main(String[] args) {
+         int[] arr={1,3,4,6,7,9,3,2,1};
+         int ans=peakIndexInMountainArray(arr);
+        System.out.println(ans);
+    }
+
+    static int peakIndexInMountainArray(int[] arr){
+        int start=0;
+        int end= arr.length-1;
+
+        while(start < end){
+            int mid=start+ (end-start)/2;
+            if(arr[mid]>arr[mid+1]){
+                end = mid;
+
+            }
+            else{
+                start=mid+1;
+            }
+        }
+
+        return start;
+    }
+}
